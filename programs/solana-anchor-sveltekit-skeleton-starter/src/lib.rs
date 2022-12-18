fecf783cbb300c925a3dde1ac072abf80e3d856b
gaylonalfano <gaylon.alfano@gmail.com>
2022-12-18 20:48:22 +0800

    All tests are passing. Have to change node to 16 in order to run the anchor run test command. Also, forgot that a new Anchor program has a generic program Id until you first build and deploy. After you deploy, you get the new programId and have to update your lib.rs file with the updated program Id. Next, need to add /pdas route for UI.

--- programs/solana-anchor-sveltekit-skeleton-starter/src/lib.rs
+++ programs/solana-anchor-sveltekit-skeleton-starter/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("6cEJsLUPZ1cx5xyagtAeS6AqBtBJKb2yijZLdVE8118V");
+
 
 #[program]
 pub mod solana_anchor_sveltekit_skeleton_starter {
