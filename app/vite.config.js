f67c9e121572fb38ef277e2f71b005f2ea4d5217
gaylonalfano <gaylon.alfano@gmail.com>
2022-12-20 11:13:30 +0800

    Updated minors.

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,13 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+  plugins: [sveltekit()],
   optimizeDeps: {
     include: ['@project-serum/anchor', '@solana/web3.js', 'buffer'],
   },
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
 };
 
 export default config;
